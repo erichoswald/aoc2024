ba173360180a62c7a57b5012b039dd9dc935189b
Erich Oswald <erich.oswald@ergon.ch>
2024-12-01 17:45:54 +0100

    Day 1: Clean up

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -11,32 +11,36 @@ fn main() {
 }
 
 fn part1(input: &str) -> i32 {
-    let mut left = Vec::new();
-    let mut right = Vec::new();
-    for line in input.lines() {
-        let mut iter = line.split_whitespace().take(2);
-        left.push(iter.next().unwrap().parse::<i32>().unwrap());
-        right.push(iter.next().unwrap().parse::<i32>().unwrap());
-    };
+    let (mut left, mut right) = parse_input(input);
     left.sort();
     right.sort();
-    left.iter().zip(right.iter()).map(|(l, r)| (l - r).abs()).sum()
+    left.iter()
+        .zip(right.iter())
+        .map(|(l, r)| (l - r).abs())
+        .sum()
 }
 
 fn part2(input: &str) -> i32 {
-    let mut left = Vec::new();
-    let mut right = Vec::new();
-    for line in input.lines() {
-        let mut iter = line.split_whitespace().take(2);
-        left.push(iter.next().unwrap().parse::<i32>().unwrap());
-        right.push(iter.next().unwrap().parse::<i32>().unwrap());
-    };
-
+    let (left, right) = parse_input(input);
     let mut appearance_counts = HashMap::new();
     for id in right.iter() {
         let entry = appearance_counts.entry(id).or_insert(0);
         *entry += 1;
     }
 
-    left.iter().fold(0, |acc, id| { acc + id * appearance_counts.get(&id).unwrap_or(&0) })
+    left.iter().fold(0, |acc, id| {
+        let appearance_count = appearance_counts.get(&id).unwrap_or(&0);
+        acc + id * appearance_count
+    })
+}
+
+fn parse_input(input: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+    for line in input.lines() {
+        let mut iter = line.split_whitespace().take(2);
+        left.push(iter.next().unwrap().parse::<i32>().unwrap());
+        right.push(iter.next().unwrap().parse::<i32>().unwrap());
+    }
+    (left, right)
 }
