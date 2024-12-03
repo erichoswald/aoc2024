6e24f0c62926ef8505b5662e6abf8647d65a45c3
Erich Oswald <erich.oswald@ergon.ch>
2024-12-03 14:36:09 +0100

    Day 3: Solution to part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,9 +1,10 @@
 use regex::Regex;
 
 fn main() {
-    let sample = include_str!("../../inputs/sample03.txt");
-    println!("Part 1 (sample): {}", part1(&sample));
-    println!("Part 2 (sample): {}\n", part2(&sample));
+    let sample01 = include_str!("../../inputs/sample03_01.txt");
+    let sample02 = include_str!("../../inputs/sample03_02.txt");
+    println!("Part 1 (sample): {}", part1(&sample01));
+    println!("Part 2 (sample): {}\n", part2(&sample02));
 
     let input = include_str!("../../inputs/day03.txt");
     println!("Part 1: {}", part1(&input));
@@ -13,14 +14,28 @@ fn main() {
 fn part1(input: &str) -> i32 {
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     input.lines().fold(0, |acc, line| {
-        re.captures_iter(line).fold(acc, |a, caps| {
-            let x = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
-            let y = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
-            a + x * y
+        re.captures_iter(line).fold(acc, |acc, caps| {
+            acc + mul(&caps)
         })
     })
 }
 
-fn part2(input: &str) -> usize {
-    0
+fn part2(input: &str) -> i32 {
+    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+    let (acc, _) = input.lines().fold((0, true), |(acc, enabled), line| {
+        re.captures_iter(line).fold((acc, enabled), |(acc, enabled), caps| {
+            match caps.get(0).unwrap().as_str() {
+                "do()" => (acc, true),
+                "don't()" => (acc, false),
+                _ => if enabled { (acc + mul(&caps), enabled) } else { (acc, enabled) }
+            }
+        })
+    });
+    acc
+}
+
+fn mul(caps: &regex::Captures) -> i32 {
+    let x = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+    let y = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+    x * y
 }
