1eb46c6ae54f53186f9fe84d7deb77aa6a638d2d
Erich Oswald <erich.oswald@ergon.ch>
2024-12-04 13:02:10 +0100

    Day 4: Even uglier solution to part 2

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -22,7 +22,16 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let lines = input.lines().collect::<Vec<&str>>();
+    let height = lines.len();
+    let width = lines.iter().map(|l| l.len()).min().unwrap();
+    let mut count = 0;
+    for y in 0..height {
+        for x in 0..width {
+            count += find_x_mas_at(&lines, x as i32, y as i32)
+        }
+    }
+    count
 }
 
 fn find_xmas_at(lines: &[&str], x: i32, y: i32) -> usize {
@@ -46,6 +55,20 @@ fn find_xmas_from(lines: &[&str], x: i32, y: i32, dx: i32, dy: i32) -> usize {
     } else {
         0
     }
+}
+
+fn find_x_mas_at(lines: &[&str], x: i32, y: i32) -> usize {
+    if is_char(lines, x, y, 'A') {
+        if is_mas_at(lines, x, y, 1, 1) && is_mas_at(lines, x, y, 1, -1) {
+            return 1;
+        }
+    }
+    0
+}
+
+fn is_mas_at(lines: &[&str], x: i32, y: i32, dx: i32, dy: i32) -> bool {
+    is_char(lines, x + dx, y + dy, 'M') && is_char(lines, x - dx, y - dy, 'S')
+     || is_char(lines, x - dx, y - dy, 'M') && is_char(lines, x + dx, y + dy, 'S')
 }
 
 fn is_char(lines: &[&str], x: i32, y: i32, ch: char) -> bool {
