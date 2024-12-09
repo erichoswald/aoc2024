472d8a465567ade1a986676b2265e467f9b42ffd
Erich Oswald <erich.oswald@ergon.ch>
2024-12-09 14:33:25 +0100

    Day 9: Add part 2 (ugly)

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -15,7 +15,9 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let mut blocks = parse_disk_map(input);
+    compact(&mut blocks[..]);
+    checksum(&blocks)
 }
 
 fn defragment(blocks: &mut [i32]) {
@@ -30,6 +32,70 @@ fn defragment(blocks: &mut [i32]) {
             tail -= 1;
         }
         head += 1;
+    }
+}
+
+fn compact(blocks: &mut [i32]) {
+    let mut segments = Vec::new();
+    let mut start = 0usize;
+    let mut end = start;
+    let mut id = blocks.first().unwrap();
+    for (index, b) in blocks.iter().enumerate() {
+        if b == id {
+            if start < end {
+                end += 1;
+            } else {
+                start = index;
+                end = index + 1;
+            }
+        } else if start < end {
+            segments.push((*id, start, end));
+            id = b;
+            start = index;
+            end = start + 1;
+        }
+    }
+    if start < end {
+        segments.push((*id, start, end));
+    }
+    segments.push((-1, end, end + 1)); // Sentinel
+
+    let mut id = *id;
+    while id >= 0 {
+        let mut source_index = 0;
+        while (segments[source_index].0 != id) {
+            source_index += 1;
+        }
+        let source = segments[source_index].clone();
+        let len = source.2 - source.1;
+        let mut target_index = 0;
+        while (target_index < segments.len() && (segments[target_index].0 >= 0 || segments[target_index].1 + len > segments[target_index].2)) {
+            target_index += 1;
+        }
+        if (target_index < segments.len() && segments[target_index].1 < source.1) {
+            let target = segments[target_index].clone();
+            segments[source_index] = (-1, source.1, source.2);
+            let end = target.1 + len;
+            let rest = target.2 - end;
+            segments[target_index] = (source.0, target.1, end);
+            if (rest > 0) {
+                let next_index = target_index + 1;
+                if segments[next_index].0 >= 0 {
+                    segments.insert(next_index, (-1, end, end + rest))
+                } else {
+                    segments[next_index] = (-1, end, segments[next_index].2)
+                }
+            }
+        }
+        id -= 1;
+    }
+
+    for segment in segments {
+        for i in segment.1..segment.2 {
+            if (i < blocks.len()) {
+                blocks[i] = segment.0
+            }
+        }
     }
 }
 
