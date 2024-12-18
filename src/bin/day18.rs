536f7fec3fedae896edaa7ba861ff893589d22ff
Erich Oswald <erich.oswald@ergon.ch>
2024-12-18 14:00:22 +0100

    Day 18: Part 2

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,13 +1,15 @@
 use aoc2024::grid::{Grid, GridPos};
 use std::cmp::Ordering;
-use std::collections::BinaryHeap;
+use std::collections::{BinaryHeap, HashSet};
 
 fn main() {
     let sample = include_str!("../../inputs/sample18.txt");
     let input = include_str!("../../inputs/day18.txt");
 
-    println!("Part1 (sample): {}", part1(sample, 7, 7, 12));
-    println!("Part1: {}", part1(input, 71, 71, 1024));
+    println!("Sample");
+    simulate(sample, 7, 7, 12);
+    println!("\nPuzzle");
+    simulate(input, 71, 71, 1024);
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
@@ -28,11 +30,25 @@ impl Ord for Path {
     }
 }
 
-fn part1(input: &str, row_count: usize, col_count: usize, bytes: usize) -> usize {
+fn simulate(input: &str, row_count: usize, col_count: usize, bytes: usize) {
     let coords = parse_coords(input);
     let mut memory = Grid::new(row_count, col_count);
     corrupt_memory(&mut memory, &coords.as_slice()[..bytes]);
-    find_shortest_path(&mut memory, GridPos::new(0, 0), GridPos::new(row_count - 1, col_count - 1))
+    let start_pos = GridPos::new(0, 0);
+    let end_pos = GridPos::new(row_count - 1, col_count - 1);
+    let steps = find_shortest_path(&mut memory, start_pos, end_pos).unwrap();
+    println!("Number of steps after {bytes} bytes: {steps}");
+
+    let mut bytes = bytes;
+    loop {
+        memory.set(coords[bytes], '#');
+        if find_shortest_path(&memory, start_pos, end_pos).is_none() {
+            break;
+        }
+        bytes += 1;
+    }
+    let last_pos = coords[bytes];
+    println!("End point unreachable after {bytes}, last bytes dropped at {},{}", last_pos.col(), last_pos.row());
 }
 
 fn corrupt_memory(memory: &mut Grid<char>, coords: &[GridPos]) {
@@ -41,22 +57,21 @@ fn corrupt_memory(memory: &mut Grid<char>, coords: &[GridPos]) {
     }
 }
 
-fn find_shortest_path(memory: &mut Grid<char>, start_pos: GridPos, end_pos: GridPos) -> usize {
+fn find_shortest_path(memory: &Grid<char>, start_pos: GridPos, end_pos: GridPos) -> Option<usize> {
+    let mut visited = HashSet::from([ start_pos ]);
     let mut queue = BinaryHeap::new();
     queue.push(Path { steps: 0, pos: start_pos });
-    memory.set(start_pos, 'O');
     while let Some(Path { steps, pos }) = queue.pop() {
         if pos == end_pos {
-            return steps;
+            return Some(steps);
         }
         for neighbour in pos.neighbours_4() {
-            if memory.is_inside(neighbour) && memory.at(neighbour) == None {
-                memory.set(neighbour, 'O');
+            if memory.is_inside(neighbour) && !memory.is_defined(neighbour) && visited.insert(neighbour) {
                 queue.push(Path { steps: steps + 1, pos: neighbour });
             }
         }
     }
-    0
+    None
 }
 
 fn parse_coords(input: &str) -> Vec<GridPos> {
